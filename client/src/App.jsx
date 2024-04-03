850b1378836bc606f388d59ac4793296a2475c1e
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-04-03 14:41:29 +0530

    recreateing repo

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Header from './components/Header'
+import SignUp from './pages/SignUp'
+
+const App = () => {
+  return (
+
+    <BrowserRouter>
+       <Header/> 
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/sign-in' element={<Signin/>}/>
+      
+        <Route path='/about' element={<About/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hello from hpme</h1>
+    </div>
+  )
+}
+
+export default Home
