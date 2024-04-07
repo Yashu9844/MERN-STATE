105605424d5377f9e226bbccc16294c1e80aba5d
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-04-07 15:58:53 +0530

    hased password added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,22 @@
 import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
+
+
 
 export const signup = async (req, res) => {
     try {
       const { username, email, password } = req.body;
   
+     const hashedPassword =  bcryptjs.hashSync(password,10);
+
+
       // Check if required fields are provided
       if (!username || !email || !password) {
         return res.status(400).json({ message: "Username, email, and password are required." });
       }
   
       // Create a new user instance
-      const newUser = new User({ username, email, password });
+      const newUser = new User({ username, email, password:hashedPassword`` });
   
       // Save the new user
       await newUser.save();
