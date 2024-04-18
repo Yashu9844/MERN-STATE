91fc11847dcccacfb7872924c6868f43183ea3b2
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-04-19 03:50:58 +0530

    delete functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -32,3 +32,23 @@ export const updateUser = async (req,res,next)=>{
     }
 
 }
+
+export const deleteUser = async (req,res,next)=>{
+    
+
+
+    if(req.user.id !== req.params.id){ // user is coming from the verify user 
+        return next(errorHandler(401,'You can only delete your account'))
+    }
+
+    try {
+   await User.findByIdAndDelete(req.params.id)
+   res.status(200).json("User has been deleted")
+        
+
+
+    } catch (error) {
+        next(error);
+    }
+
+}
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,11 +3,14 @@ import {useSelector} from 'react-redux'
 import { useRef } from 'react'
 import {getDownloadURL, getStorage,ref, uploadBytesResumable} from 'firebase/storage'
 import { app } from '../firebase'
-import { updateUserFailure,updateUserStart,updateUserSuccess } from '../redux/user/userSlice'
+import { updateUserFailure,updateUserStart,updateUserSuccess,deleteUserFailure,deleteUserStart,deleteUserSuccess } from '../redux/user/userSlice'
 import { useDispatch } from 'react-redux'
 
 
+
 const Profile = () => {
+ 
+
   const dispatch = useDispatch();
   const {currentUser,loading,error} = useSelector((state) => state.user);
   const [file,setFile] = useState(null)
@@ -85,6 +88,25 @@ const Profile = () => {
   };
 
 
+const handleDeleteUser = async ()=>{
+  try {
+    dispatch(deleteUserStart());
+    const res = await fetch(`/api/user/delete/${currentUser._id}`,{
+      method:'DELETE',
+    });
+    const data = res.json();
+    if(data.success === false){
+      dispatch(deleteUserFailure(data.message))
+    }
+    dispatch(deleteUserSuccess(data));
+
+    
+  } catch (error) {
+   dispatch(deleteUserFailure(error.message))
+    
+  }
+}
+  
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -128,7 +150,7 @@ const Profile = () => {
 </form>
 <div className="flex justify-between mt-3
 ">
-  <span className='text-red-700 cursor-pointer'>Delete account</span>
+  <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete account</span>
   <span  className='text-red-700 cursor-pointer'>Sign out</span>
 </div>
     <div className="flex justify-center ">
