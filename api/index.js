fc6ccac0f53727f1c25e1e3cf15faaa7301b9fea
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-04-29 13:10:52 +0530

    third commit

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 })
 
-const _dirname = path.resolve();
+const __dirname = path.resolve();
 
 
 const app = express();
@@ -32,11 +32,12 @@ app.use("/api/user",userRouter)
 app.use("/api/auth",authRouter)
 app.use("/api/listing",listing)
 
-app.use(express.static(path.join(_dirname,'/client/dist')))
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(_dirname,'client','dist','index.html'))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
+
 app.use((err,req,res,next)=>{
 
  const statusCode = err.statusCode || 500;
